7de0cf4ea4765b86e15f161acce16a01118a13a9
Supratik Das <dassupratik86@gmail.com>
2024-11-09 21:03:17 +0530

    ss

--- src/data/index.ts
+++ src/data/index.ts
@@ -12,27 +12,27 @@ export const navItems = [
 export const upperTechStackList = [
   {
     name: "ReactJS",
-    icon: "/react.svg"
+    icon: "./react.svg"
   },
   {
     name: "NodeJS",
-    icon: "/node.svg"
+    icon: "./node.svg"
   },
   {
     name: "Express",
-    icon: "/express.svg"
+    icon: "./express.svg"
   },
   {
     name: "MongoDB",
-    icon: "/mongo.svg"
+    icon: "./mongo.svg"
   },
   {
     name: "JavaScript",
-    icon: "/js.svg"
+    icon: "./js.svg"
   },
   {
     name: "TailWind CSS",
-    icon: "/tss.svg"
+    icon: "./tss.svg"
   },
   
 ]
@@ -40,27 +40,27 @@ export const upperTechStackList = [
 export const lowerTechStackList = [
   {
     name: "HTML5",
-    icon: "/html.svg"
+    icon: "./html.svg"
   },
   {
     name: "CSS3",
-    icon: "/css.svg"
+    icon: "./css.svg"
   },
   {
     name: "Next JS",
-    icon: "/next.svg"
+    icon: "./next.svg"
   },
   {
     name: "React Redux",
-    icon: "/redux.svg"
+    icon: "./redux.svg"
   },
   {
     name: "Framer Motion",
-    icon: "/fm.svg"
+    icon: "./fm.svg"
   },
   {
     name: "Git",
-    icon: "/hub.svg"
+    icon: "./hub.svg"
   },
 ]
 
@@ -98,16 +98,16 @@ export const projects = [
     id: 1,
     title: "ZMeet",
     des: "Simplify your video conferencing experience with ZMeet. Seamlessly connect with colleagues and friends and schedule meetings",
-    img: "/Zmeet.jpg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    img: "./Zmeet.jpg",
+    iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./stream.svg", "./c.svg"],
     link: "https://z-meet-acro.vercel.app/",
   },
   {
     id: 2,
     title: "JEE-PYQ",
     des: "A Software to store previous year questions of Join Entrance Examinations",
-    img: "/jee.jpg",
-    iconLists: ["/re.svg", "/js.svg", "/nodejs.svg"],
+    img: "./jee.jpg",
+    iconLists: ["./re.svg", "./js.svg", "./nodejs.svg"],
     link: "https://jeepyq.vercel.app/",
   },
 
@@ -123,14 +123,14 @@ export const workExperience = [
     title: "Frontend Developer",
     desc: "Lead a Five Member Team for College Hackathon",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "./exp1.svg",
   },
   {
     id: 2,
     title: "Data Structures And Algorithms",
     desc: "Proficient in implementing and optimizing data structures and algorithms in Java for efficient problem-solving.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    thumbnail: "./exp2.svg",
   },
 
 ];
@@ -138,17 +138,17 @@ export const workExperience = [
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "./git.svg",
     link: "https://github.com/acroop"
   },
   {
     id: 2,
-    img: "/insta.svg",
+    img: "./insta.svg",
     link: 'https://www.instagram.com/acro_gamer_op'
   },
   {
     id: 3,
-    img: "/link.svg",
+    img: "./link.svg",
     link: "https://www.linkedin.com/in/supratik-das86/"
   },
 ];
